7cb273fd8ca18620afbe2601beadd3c5d1c4c161
Lucas da Silva <lucas.2601@gmail.com>
2024-08-23 20:58:54 -0300

    Endpoint ultimo partida disputada

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -20,6 +21,18 @@ type Team struct {
 	Nome          string `json:"nome"`
 	Apelido       string `json:"apelido"`
 	Sigla         string `json:"sigla"`
+}
+
+type Match struct {
+	Id                        int       `json:"id"`
+	Data                      time.Time `json:"data"`
+	Clube_Mandante            string    `json:"clube_mandante"`
+	Placar_Mandante           int       `json:"placar_mandante"`
+	Placar_Mandante_Penaltis  int       `json:"placar_mandante_penaltis"`
+	Clube_Visitante           string    `json:"clube_visitante"`
+	Placar_Visitante          int       `json:"placar_visitante"`
+	Placar_Visitante_Penaltis int       `json:"placar_visitante_penaltis"`
+	Campeonato                string    `json:"campeonato"`
 }
 
 func teamHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
@@ -51,6 +64,71 @@ func teamHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	json.NewEncoder(w).Encode(team)
 }
 
+func lastMatchPlayedHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	w.Header().Set("Contenty-Type", "application/json")
+
+	vars := mux.Vars(r)
+	idTeamString := vars["id"]
+
+	idTeam, err := strconv.Atoi(idTeamString)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	query := `SELECT
+				jogos.data, 
+				campeonatos.nome AS 'campeonato',
+				mandante.nome AS 'clube_mandante', 
+				jogos.placar_mandante,
+				jogos.placar_mandante_penaltis,
+				visitante.nome AS 'clube_visitante',
+				jogos.placar_visitante,
+				jogos.placar_visitante_penaltis
+			FROM jogos
+			INNER JOIN campeonatos ON campeonatos.id = jogos.id_campeonato
+			INNER JOIN clubes AS mandante ON mandante.id = jogos.id_clube_mandante
+			INNER JOIN clubes AS visitante ON visitante.id = jogos.id_clube_visitante
+			WHERE
+				jogos.data < NOW() AND
+				(
+					jogos.id_clube_mandante = ? OR 
+					jogos.id_clube_visitante = ?
+				)
+			ORDER BY jogos.data DESC
+			LIMIT 1`
+	row := db.QueryRow(query, idTeam, idTeam)
+
+	var dataStr string
+	var lastMatchPlayed Match
+	if err := row.Scan(
+		&dataStr,
+		&lastMatchPlayed.Campeonato,
+		&lastMatchPlayed.Clube_Mandante,
+		&lastMatchPlayed.Placar_Mandante,
+		&lastMatchPlayed.Placar_Mandante_Penaltis,
+		&lastMatchPlayed.Clube_Visitante,
+		&lastMatchPlayed.Placar_Visitante,
+		&lastMatchPlayed.Placar_Visitante_Penaltis); err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "Último jogo não encontrado", http.StatusNotFound)
+		} else {
+			fmt.Printf("Database error: %v\n", err)
+			http.Error(w, "Database error", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	lastMatchPlayed.Data, err = time.Parse("2006-01-02 15:04:05", dataStr)
+	if err != nil {
+		fmt.Printf("Time parsing error: %v\n", err)
+		http.Error(w, "Erro ao processar a data", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(lastMatchPlayed)
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -77,6 +155,10 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/time/{id}", func(w http.ResponseWriter, r *http.Request) {
 		teamHandler(w, r, db)
+	})
+
+	r.HandleFunc("/time/{id}/lastMatchPlayed", func(w http.ResponseWriter, r *http.Request) {
+		lastMatchPlayedHandler(w, r, db)
 	})
 
 	fmt.Println("Server is running")
